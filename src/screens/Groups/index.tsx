df27c6f648d46c3e56e3108f6058590eb943ef32
MarcosVel <marcos.veloso@sempreceub.com>
2022-11-20 17:43:27 -0300

    feat: add header component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,11 @@
+import Header from "@components/Header";
 import React from "react";
 import { Container } from "./styles";
 
 export default function Groups() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
 }
