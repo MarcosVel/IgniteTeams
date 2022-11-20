9829bae00ab756fc91e85b847f1aaf88d4d49e94
MarcosVel <marcos.veloso@sempreceub.com>
2022-11-20 19:17:21 -0300

    feat: list of groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,33 @@
 import GroupCard from "@components/GroupCard";
 import Header from "@components/Header";
 import Highlight from "@components/Highlight";
-import React from "react";
+import React, { useState } from "react";
+import { FlatList } from "react-native";
 import { Container } from "./styles";
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    "Galera da Rocket",
+    "Amigos",
+    "Curso JavaScript",
+    "React Native",
+    "Família",
+  ]);
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
 
-      <GroupCard title="Galera do Ignite" onPress={() => alert("Clicou")} />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={{
+          paddingBottom: 32,
+        }}
+      />
     </Container>
   );
 }
