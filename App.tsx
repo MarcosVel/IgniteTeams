059da1be35d21ac898009b2cd70673673f46697e
MarcosVel <marcos.veloso@sempreceub.com>
2022-11-20 17:08:58 -0300

    feat: add loading component

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
-import Groups from "@screens/Groups";
-import theme from "./src/theme";
-import { ThemeProvider } from "styled-components/native";
+import Loading from "@components/Loading";
 import {
-  useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
+  useFonts,
 } from "@expo-google-fonts/roboto";
-import { ActivityIndicator } from "react-native";
+import Groups from "@screens/Groups";
+import { ThemeProvider } from "styled-components/native";
+import theme from "./src/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
