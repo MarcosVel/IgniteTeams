ddda6fb150d006cc17c02050832fe2d8595de4b8
MarcosVel <marcos.veloso@sempreceub.com>
2022-11-25 14:52:32 -0300

    feat: filter style

--- src/screens/Users/index.tsx
+++ src/screens/Users/index.tsx
@@ -1,4 +1,5 @@
 import ButtonIcon from "@components/ButtonIcon";
+import Filter from "@components/Filter";
 import Header from "@components/Header";
 import Highlight from "@components/Highlight";
 import Input from "@components/Input";
@@ -21,6 +22,8 @@ export default function Users() {
           <Input placeholder="Nome da pessoa" autoCorrect={false} />
           <ButtonIcon icon="add" />
         </Form>
+
+        <Filter title="Time A" />
       </Container>
     </TouchableWithoutFeedback>
   );
