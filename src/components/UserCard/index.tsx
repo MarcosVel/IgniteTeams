61e91eb81a61f9558ae98117da8b16fccb92e447
MarcosVel <marcos.veloso@sempreceub.com>
2022-11-29 22:23:40 -0300

    feat: listing users and fix scroll

--- src/components/UserCard/index.tsx
+++ src/components/UserCard/index.tsx
@@ -1,13 +1,16 @@
 import ButtonIcon from "@components/ButtonIcon";
 import React from "react";
 import { Container, Icon, Name } from "./styles";
+import { TouchableWithoutFeedback } from "react-native";
 
-export default function UserCard({ name }) {
+export default function UserCard({ name, onRemove }) {
   return (
-    <Container>
-      <Icon name="person" />
-      <Name numberOfLines={1}>{name}</Name>
-      <ButtonIcon icon="close" type="SECONDARY" />
-    </Container>
+    <TouchableWithoutFeedback>
+      <Container>
+        <Icon name="person" />
+        <Name numberOfLines={1}>{name}</Name>
+        <ButtonIcon icon="close" type="SECONDARY" />
+      </Container>
+    </TouchableWithoutFeedback>
   );
 }
