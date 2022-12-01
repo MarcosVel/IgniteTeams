07c845007b91c6fb262667a27ff8f67eddd2ac74
MarcosVel <marcos.veloso@sempreceub.com>
2022-11-30 22:40:27 -0300

    feat: configuring routes

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,16 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Groups from "@screens/Groups";
+import NewGroup from "@screens/NewGroup";
+import Users from "@screens/Users";
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppRoutes() {
+  return (
+    <Navigator screenOptions={{ headerShown: false }}>
+      <Screen name="groups" component={Groups} />
+      <Screen name="new" component={NewGroup} />
+      <Screen name="users" component={Users} />
+    </Navigator>
+  );
+}
