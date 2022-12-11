2629188d77b660fef920a808236cf025982518ce
MarcosVel <marcos.veloso@sempreceub.com>
2022-12-11 18:12:26 -0300

    feat: remove user from team

--- src/components/UserCard/index.tsx
+++ src/components/UserCard/index.tsx
@@ -3,13 +3,18 @@ import React from "react";
 import { Container, Icon, Name } from "./styles";
 import { TouchableWithoutFeedback } from "react-native";
 
-export default function UserCard({ name, onRemove }) {
+type Props = {
+  name: string;
+  onRemove: () => void;
+};
+
+export default function UserCard({ name, onRemove }: Props) {
   return (
     <TouchableWithoutFeedback>
       <Container>
         <Icon name="person" />
         <Name numberOfLines={1}>{name}</Name>
-        <ButtonIcon icon="close" type="SECONDARY" />
+        <ButtonIcon icon="close" type="SECONDARY" onPress={onRemove} />
       </Container>
     </TouchableWithoutFeedback>
   );
